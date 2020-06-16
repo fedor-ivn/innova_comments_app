9409c0dd8625fa1e30d8e5fac4f0da1392edb8d7
Ivanov Fedor <ivnfedor@gmail.com>
2020-06-16 09:03:50 +0300

    Add too many parents validation

--- main/serializers.py
+++ main/serializers.py
@@ -6,3 +6,11 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Comment
         fields = '__all__'
+
+    def validate(self, data):
+        parent = data.get('parent')
+        if parent.parent is not None:
+            raise serializers.ValidationError(
+                'Parent comment must not have parent'
+            )
+        return data
