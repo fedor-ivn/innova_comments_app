ce6dc67876cd068e5b41a47f4e44aed38a62403b
Ivanov Fedor <ivnfedor@gmail.com>
2020-06-22 08:41:28 +0300

    Added tree ordered Comment model. Switch to MPTT

--- main/serializers.py
+++ main/serializers.py
@@ -6,13 +6,5 @@ class CommentSerializer(serializers.ModelSerializer):
     sort_key = serializers.CharField(read_only=True)
 
     class Meta:
-        model = models.Comment
+        model = models.MPTTComment
         fields = '__all__'
-
-    def validate(self, data):
-        parent = data.get('parent')
-        if parent and parent.parent:
-            raise serializers.ValidationError(
-                'Parent comment must not have parent'
-            )
-        return data
